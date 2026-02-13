2a8d49830f4109bf85e1a980b4bddfe291559a72
Kunal Deo <kunaldeo@Kunals-MacBook-Air-648.local>
2026-02-13 15:09:06 +0530

    Updated project

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args) {
+        int number = 29; 
+
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
